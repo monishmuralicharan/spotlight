21ad6241d9fe4652ff49ab5a3755976f4a024dcf
monish237 <monish.muralicharan@gmail.com>
2025-01-22 23:09:50 -0500

    Initial Commit

---
+++ spotlight/spotlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotlightApp.swift
+//  spotlight
+//
+//  Created by Monish on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct spotlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
